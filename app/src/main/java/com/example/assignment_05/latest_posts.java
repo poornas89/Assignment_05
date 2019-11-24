94038315935484ccb60d41aa5bf07929da7912b7
poornas89 <poorna_sanjeewa@yahoo.com>
2019-11-24 22:10:12 +0530

    Completed Assignment 05

---
+++ app/src/main/java/com/example/assignment_05/latest_posts.java
@@ -0,0 +1,14 @@
+package com.example.assignment_05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class latest_posts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latest_posts);
+    }
+}
--- app/src/main/java/com/example/assignment_05/login_success.java
+++ app/src/main/java/com/example/assignment_05/login_success.java
@@ -2,13 +2,100 @@ package com.example.assignment_05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_success extends AppCompatActivity {
+
+    private Button latest_posts;
+    private Button problems;
+    private Button newsandevnts;
+    private Button doations;
+    private Button succsess;
+    private Button find_loc;
+    private Button abt_us;
+    private Button cnt_us;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_success);
+        latest_posts = findViewById(R.id.latest_posts);
+        problems = findViewById(R.id.problems);
+        newsandevnts = findViewById(R.id.newsandevnts);
+        doations = findViewById(R.id.doations);
+        succsess = findViewById(R.id.succsess);
+        find_loc = findViewById(R.id.find_loc);
+        abt_us = findViewById(R.id.abt_us);
+        cnt_us = findViewById(R.id.cnt_us);
+
+        latest_posts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, com.example.assignment_05.latest_posts.class);
+                startActivity(intent);
+            }
+        });
+
+
+        problems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, com.example.assignment_05.problems.class);
+                startActivity(intent);
+            }
+        });
+
+        newsandevnts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, com.example.assignment_05.newsandevnts.class);
+                startActivity(intent);
+            }
+        });
+
+        doations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, com.example.assignment_05.doations.class);
+                startActivity(intent);
+            }
+        });
+
+        succsess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, success.class);
+                startActivity(intent);
+            }
+        });
+
+        find_loc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, findloc.class);
+                startActivity(intent);
+            }
+        });
+
+        abt_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, aboutus.class);
+                startActivity(intent);
+            }
+        });
+
+        cnt_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_success.this, contactus.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
